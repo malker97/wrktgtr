09403fdd289650a911daf549e8e35f9b94f6f9f9
Zhengmao <malker@qq.com>
2020-05-06 02:24:19 -0700

    我还没有完成这一部分，我只是上传了css和html的代码，js部分都没啥东西

--- assign3/1-form.js
+++ assign3/1-form.js
@@ -1,1 +1,23 @@
 // Enter your code here
+/*
+name: spongebob squarepants
+email: spongebob@spongebob.me
+registration status: waitlist
+class section: undergrad
+courses: operating systems, fullstack web development
+class goals: learning more about frontend!
+*/
+function returninfo{
+    var name=document.getElementById("fullname");
+    var email=document.getElementById("email");
+    var rigsttu=document.getElementById("rigsttu");
+    var classlect=document.getElementById("classlect");
+    var anycoures=document.getElementById("anycoures");
+    var anyels=document.getElementById("anyels");
+    console.log("Name:" + name);
+    console.log("Email:" + email);
+    console.log("registration status:" + rigsttu);
+    console.log("class section:" + classlect);
+    console.log("courses:" + anycoures);
+    console.log("class goals" + anyels);
+}
