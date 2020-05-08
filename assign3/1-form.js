e349acc5a748312d68ba07a1b71b4dd3a050b584
Zhengmao <malker@qq.com>
2020-05-07 22:33:15 -0700

    守望先锋全几把是挂，我就只能来写作业了，QAQ太惨了

--- assign3/1-form.js
+++ assign3/1-form.js
@@ -7,17 +7,18 @@ class section: undergrad
 courses: operating systems, fullstack web development
 class goals: learning more about frontend!
 */
-function returninfo{
-    var name=document.getElementById("fullname");
-    var email=document.getElementById("email");
-    var rigsttu=document.getElementById("rigsttu");
-    var classlect=document.getElementById("classlect");
-    var anycoures=document.getElementById("anycoures");
-    var anyels=document.getElementById("anyels");
+document.getElementById("sumb").onclick = function() {myFunction()};
+function myFunction() {
+    var name = document.getElementById("fullname");
+    var email = document.getElementById("email");
+    var rigsttu = document.getElementById("rigsttu");
+    var classlect = document.getElementById("classlect");
+    var anycoures = document.getElementById("anycoures");
+    var anyels = document.getElementById("anyels");
     console.log("Name:" + name);
     console.log("Email:" + email);
     console.log("registration status:" + rigsttu);
     console.log("class section:" + classlect);
     console.log("courses:" + anycoures);
-    console.log("class goals" + anyels);
+    console.log("class goals:" + anyels);
 }
