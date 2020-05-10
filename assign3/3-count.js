bdcfcb9b04e92bcca081110e38dd3cb329d16331
Zhengmao <malker@qq.com>
2020-05-09 20:32:51 -0700

    第三部分搞定了

--- assign3/3-count.js
+++ assign3/3-count.js
@@ -1,5 +1,16 @@
 // Enter your code here
-document.getElementById("find").onclick=function () {
-        var intro = document.getElementById("intro").value;
-        var;
+function fndwdandhlgt(str, key) {
+        var reg = new RegExp("(" + key + ")", "g");
+        var newstr = str.replace(reg,"<font style='background:#ff0;'>$1</font>");
+        return newstr;
+}
+function myfunction() {
+        var oldstrid = document.getElementById("intro");
+        var oldstr = oldstrid.innerHTML;
+        var keyinput = document.getElementById("input");
+        var key=keyinput.value;
+        var newstr = fndwdandhlgt(oldstr,key);
+//        alert(newstr);
+        document.getElementById("intro").innerHTML=newstr;
+        // alert("hear");
 }
